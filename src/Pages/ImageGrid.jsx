19e2d8627572f9c1283a562bfcd968338d1ead80
Rana Usman <usman4243ch@gmail.com>
2024-05-13 16:45:33 -0700

    Add Images in Main Page

--- src/Pages/ImageGrid.jsx
+++ src/Pages/ImageGrid.jsx
@@ -1,4 +1,10 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import img1 from "../assets/img1.jpeg"
+import img2 from "../assets/img2.jpeg"
+import img3 from "../assets/img3.jpeg"
+import img4 from "../assets/img4.jpeg"
+import img5 from "../assets/img5.jpeg"
 
 const ImageGrid = () => {
     return (
@@ -6,37 +12,44 @@ const ImageGrid = () => {
             <div className='grid grid-cols-12 grid-rows-5 md:grid-rows-2 gap-y-2 '>
                 <div className='md:col-span-4 col-span-12 row-span-2 bg-orange-600 max-h-[400px]'>
                     <img
-                        src='https://picsum.photos/200/300'
+                        src={img5}
                         className='w-full h-full  bg-cover bg-center'
-                        alt=''
+                        alt='user'
                     />
                 </div>
-                <div className='md:col-span-5 col-span-12 row-span-2 bg-red-500 flex justify-center items-center'>
-                    <h2 className='text-white'>Hllo ther</h2>
+                <div className='md:col-span-5 col-span-12 row-span-2 bg-[#404142] flex flex-col justify-center'>
+                    <div>
+                        <p className='text-[#f2f2f2] font-semibold px-16'>At Enfotrix, we're on a mission to inspire creativity and empower
+                            individuals to thrive in the digital world. Since 2019, we've been
+                            offering top-notch courses, services, and solutions in technology
+                            and design. With a focus on excellence, creativity, and continuous
+                            learning, we've helped countless clients and students achieve
+                            their goals. Meet our dedicated team who's passionate about
+                            making a difference in the world of technology and design.</p>
+                    </div>
+                    <div>
+                        <Link><p className='uppercase text-[#ffba21] text-sm pl-16 mt-10'>read more</p></Link>
+                    </div>
                 </div>
-                <div className='md:col-span-3 col-span-12 row-span-1 bg-blue-600 flex justify-center items-center text-white '>
-                    <h1 className='text-5xl'>Hello WOrld</h1>
+                <div className='md:col-span-3 col-span-12 row-span-1 bg-[#0047AB] flex justify-center items-center text-white '>
+                    <h1 className='text-4xl font-bold uppercase'>ceo <br />enfotrix</h1>
                 </div>
             </div>
             <div className='grid grid-cols-12 md:grid-cols-3 md:grid-rows-2 grid-rows-6 gap-y-2 mt-2 gap-x-2'>
                 <div className='md:col-span-1 col-span-6 row-span-1 bg-orange-600 max-h-[400px]'>
-                    <img
-                        src='https://picsum.photos/200/300'
-                        className='w-full h-full  bg-cover bg-center'
-                        alt=''
-                    />
+                    <img src={img1} alt="user" />
                 </div>
                 <div className='md:col-span-1 col-span-6 row-span-2 bg-red-500 flex justify-center items-center'>
                     <h2 className='text-white'>Hllo ther</h2>
                 </div>
                 <div className='md:col-span-1 col-span-6 row-span-1 bg-blue-600 flex justify-center items-center text-white '>
-                    <h1 className='text-5xl'>Hello WOrld</h1>
+                    <img src={img2} alt="user" />
                 </div>
                 <div className='md:col-span-1 col-span-6 row-span-1 bg-blue-600 flex justify-center items-center text-white '>
-                    <h1 className='text-5xl'>Hello WOrld</h1>
+                    <img src={img3} alt="user" />
                 </div>
                 <div className='md:col-span-1 col-span-6 row-span-1 bg-blue-600 flex justify-center items-center text-white '>
-                    <h1 className='text-5xl'>Hello WOrld</h1>
+                    <img src={img4} alt="user" />
                 </div>
             </div>
             <div className='grid grid-cols-12 grid-rows-5 md:grid-rows-2 gap-y-2 mt-2 gap-x-2'>
