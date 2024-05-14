82fc5b5312ebe85d2670157184020ebc777203a6
Rana Usman <usman4243ch@gmail.com>
2024-05-13 17:01:13 -0700

    Add imges in testimonail section

--- src/component/Testimonial.jsx
+++ src/component/Testimonial.jsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import img6 from "../assets/img6.jpeg"
+import img7 from "../assets/img7.jpeg"
+import img1 from "../assets/img1.jpeg"
 
 const Testimonial = () => {
     return (
@@ -9,7 +12,7 @@ const Testimonial = () => {
             <div className='grid grid-cols-1 gap-2 md:grid-cols-2 lg:grid-cols-3 '>
                 <div className='bg-[#f2f2f2]'>
                     <div className='flex justify-center items-center bg-gray-200 h-[300px] '>
-                        video
+                        <img src={img6} alt="user" />
                     </div>
                     <div className='py-5 text-[#414042]'>
                         <h3 className='font-bold text-xl text-center'>UMER ASLAM</h3>
@@ -18,7 +21,7 @@ const Testimonial = () => {
                 </div>
                 <div className='pt-10 md:pt-0 bg-[#f2f2f2]'>
                     <div className='flex justify-center items-center bg-gray-200 h-[300px] '>
-                        video
+                        <img src={img7} alt="user" />
                     </div>
                     <div className='py-5 text-[#414042]'>
                         <h3 className='font-bold text-xl text-center'>ALI AKBAR</h3>
@@ -27,7 +30,7 @@ const Testimonial = () => {
                 </div>
                 <div className='pt-10 lg:pt-0 bg-[#f2f2f2]'>
                     <div className='flex justify-center items-center bg-gray-200 h-[300px] '>
-                        video
+                        <img src={img1 } alt="user" />
                     </div>
                     <div className='py-5 text-[#414042]'>
                         <h3 className='font-bold text-xl text-center'>SAMI ULLAH AWAN</h3>
